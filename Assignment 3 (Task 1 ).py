fd554e4c9a665943e41c108c3cc7dd790377d248
strux12 <savithreddyr@gmail.com>
2025-08-19 21:30:23 +0530

    Add files via upload

---
+++ Assignment 3 (Task 1 ).py
@@ -0,0 +1,10 @@
+num=int(input("Enter a number :"))
+def factorial (n):
+    if n ==0 or n==1:
+     return 1
+    else:
+      return n * factorial(n-1)
+
+
+print("Factorial of the number ", num,"is",factorial(num))
+
---
+++ Assignment(Task 2).py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number :"))
+import math
+print("Square root is ", math.sqrt(num))
+print("Logarithm :" , math.log(num))
+print("Sine",math.sin(num))
